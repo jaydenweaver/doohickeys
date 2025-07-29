40f73f036ba359b34a213a76cfc975a156c81c23
jaydenweaver <jayden_weaver@hotmail.com>
2025-07-29 15:21:51 +1000

    made clearer

--- bloom-filter/BloomFilter.cpp
+++ bloom-filter/BloomFilter.cpp
@@ -25,6 +25,7 @@ optimal number of hash functions is calculated via the following:
 
 #include <cmath>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -38,11 +39,11 @@ constexpr double LN_2_SQUARED = 0.48045301391;
 
 bool parse_arg(const char *arg, int &n);
 
-int get_bit_array_size(const int &n, const double &p) {
+int get_bit_array_size(const int n, const double p) {
   return static_cast<int>(std::ceil(-((n * std::log(p)) / LN_2_SQUARED)));
 }
 
-int get_num_hashes(const int &m, const int &n) {
+int get_num_hashes(const int m, const int n) {
   return static_cast<int>(std::ceil(((double)m / n) * LN_2));
 }
 
@@ -52,12 +53,12 @@ int main(int argc, char *argv[]) {
 
   if (argc > 1) {
     if (!parse_arg(argv[1], n))
-      return 0;
+      return EXIT_FAILURE;
   }
 
   const int bit_array_size = get_bit_array_size(n, p);
   const int num_hashes = get_num_hashes(bit_array_size, n);
-  const int buckets = std::ceil((double)bit_array_size / 64);
+  const int buckets = (bit_array_size + 63) / 64;
 
   std::vector<uint64_t> bits(buckets);
 
