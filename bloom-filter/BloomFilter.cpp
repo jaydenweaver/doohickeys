77f5085259b36a26937eca1dd403afee8834df9e
jaydenweaver <jayden_weaver@hotmail.com>
2025-07-29 13:20:02 +1000

    seperate parse_arg()

--- bloom-filter/BloomFilter.cpp
+++ bloom-filter/BloomFilter.cpp
@@ -4,23 +4,38 @@
 #include <string>
 #include <vector>
 
-#define DEFAULT_SIZE 1000
+#define DEFAULT_N 1000
+#define DEFAULT_BITS 1000000
+
+bool parse_arg(const char *arg, int &n);
 
 int main(int argc, char *argv[]) {
   std::vector<uint64_t> bits;
-  int n = DEFAULT_SIZE;
+  int n = DEFAULT_N;
 
   if (argc > 1) {
-    try {
-      n = stoi(std::string(argv[1]));
-    } catch (const std::invalid_argument &e) {
-      std::cerr << "invalid argument!" << std::endl;
+    if (!parse_arg(argv[1], n))
       return 0;
-    } catch (const std::out_of_range &e) {
-      std::cerr << "number for 'n' too large!" << std::endl;
-      return 0;
-    }
   }
   std::cout << "n: " << n << std::endl;
   return 0;
+}
+
+bool parse_arg(const char *arg, int &n) {
+  try {
+    n = stoi(std::string(arg));
+  } catch (const std::invalid_argument &e) {
+    std::cerr << "invalid argument!" << std::endl;
+    return false;
+  } catch (const std::out_of_range &e) {
+    std::cerr << "number for 'n' too large!" << std::endl;
+    return false;
+  }
+
+  if (n < 0) {
+    std::cerr << "'n' cannot be negative!" << std::endl;
+    return false;
+  }
+
+  return true;
 }
