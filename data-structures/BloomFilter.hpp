804fd248adec1cb2d21e8fa4a2d14156118218e0
jaydenweaver <jayden_weaver@hotmail.com>
2025-07-30 16:17:50 +1000

    translate to header-only

--- data-structures/BloomFilter.hpp
+++ data-structures/BloomFilter.hpp
@@ -1,6 +1,7 @@
+#pragma once
 /*
 
-COUNTING BLOOM FILTER IMPLEMENTATION - currently only supports strings
+BLOOM FILTER IMPLEMENTATION - currently only supports strings
 ---------------------------------------------------------
 
 optimal bit array size is calculated via the following:
@@ -33,38 +34,37 @@ optimal number of hash functions is calculated via the following:
 constexpr double LN_2 = 0.69314718056;
 constexpr double LN_2_SQUARED = 0.48045301391;
 
-class CountingBloomFilter {
+class BloomFilter {
 private:
-  int array_size;
+  int bit_array_size;
   int num_hashes;
   int buckets;
   std::vector<uint64_t> bits;
 
-  void operate(bool remove, const std::string &value);
   // uses a standard double hash
   std::vector<uint64_t> get_hash_indexes(const std::string &value) const;
 
 public:
-  CountingBloomFilter(int n, double p);
+  BloomFilter(int n, double p);
 
-  void insert(const std::string &value) { operate(false, value); }
-  void remove(const std::string &value) { operate(true, value); }
+  void insert(const std::string &value);
   bool contains(const std::string &value) const;
 };
 
 // n must be greater than 0, p must be within range, 0 -> 1
-CountingBloomFilter::CountingBloomFilter(int n, double p)
-    : array_size(
+BloomFilter::BloomFilter(int n, double p)
+    : bit_array_size(
           static_cast<int>(std::ceil(-((n * std::log(p)) / LN_2_SQUARED)))),
-      num_hashes(static_cast<int>(std::ceil(((double)array_size / n) * LN_2))),
-      buckets((array_size + 7) / 8), bits(buckets) {
+      num_hashes(
+          static_cast<int>(std::ceil(((double)bit_array_size / n) * LN_2))),
+      buckets((bit_array_size + 63) / 64), bits(buckets) {
   if (n <= 0 || p <= 0.0 || p >= 1.0) {
     throw std::invalid_argument("invalid input: n must be > 0, 0 < p < 1");
   }
 }
 
 std::vector<uint64_t>
-CountingBloomFilter::get_hash_indexes(const std::string &value) const {
+BloomFilter::get_hash_indexes(const std::string &value) const {
   std::vector<uint64_t> indexes;
   indexes.reserve(num_hashes);
 
@@ -72,41 +72,30 @@ CountingBloomFilter::get_hash_indexes(const std::string &value) const {
   uint64_t h2 = std::hash<std::string>{}("salt" + value);
 
   for (int i = 0; i < num_hashes; i++) {
-    indexes.emplace_back((h1 + i * h2) % array_size);
+    indexes.emplace_back((h1 + i * h2) % bit_array_size);
   }
 
   return indexes;
 }
 
-void CountingBloomFilter::operate(bool remove, const std::string &value) {
+void BloomFilter::insert(const std::string &value) {
   std::vector<uint64_t> indexes = get_hash_indexes(value);
   for (uint64_t index : indexes) {
-    int bucket = index / 8;
-    int offset = (index % 8) * 8;
-
-    uint8_t count = (bits[bucket] >> offset) & 0xFF;
-
-    if (remove && count > 0)
-      count--;
-    else if (!remove && count != 255)
-      count++;
-    else
-      continue;
-
-    bits[bucket] &= ~(0xFFULL << offset);
-    bits[bucket] |= (static_cast<uint64_t>(count) << offset);
+    int bucket = index / 64;
+    int index_in_bucket = index % 64;
+    bits[bucket] |= (1ULL << index_in_bucket);
   }
 }
 
-bool CountingBloomFilter::contains(const std::string &value) const {
+bool BloomFilter::contains(const std::string &value) const {
   std::vector<uint64_t> indexes = get_hash_indexes(value);
   for (uint64_t index : indexes) {
-    int bucket = index / 8;
-    int offset = (index % 8) * 8;
+    int bucket = index / 64;
+    int index_in_bucket = index % 64;
 
-    uint8_t count = (bits[bucket] >> offset) & 0xFF;
-    if (count == 0)
+    if ((bits[bucket] & (1ULL << index_in_bucket)) == 0) {
       return false;
+    }
   }
   return true;
 }
--- data-structures/CountingBloomFilter.hpp
+++ data-structures/CountingBloomFilter.hpp
@@ -1,6 +1,7 @@
+#pragma once
 /*
 
-BLOOM FILTER IMPLEMENTATION - currently only supports strings
+COUNTING BLOOM FILTER IMPLEMENTATION - currently only supports strings
 ---------------------------------------------------------
 
 optimal bit array size is calculated via the following:
@@ -33,37 +34,38 @@ optimal number of hash functions is calculated via the following:
 constexpr double LN_2 = 0.69314718056;
 constexpr double LN_2_SQUARED = 0.48045301391;
 
-class BloomFilter {
+class CountingBloomFilter {
 private:
-  int bit_array_size;
+  int array_size;
   int num_hashes;
   int buckets;
   std::vector<uint64_t> bits;
 
+  void operate(bool remove, const std::string &value);
   // uses a standard double hash
   std::vector<uint64_t> get_hash_indexes(const std::string &value) const;
 
 public:
-  BloomFilter(int n, double p);
+  CountingBloomFilter(int n, double p);
 
-  void insert(const std::string &value);
+  void insert(const std::string &value) { operate(false, value); }
+  void remove(const std::string &value) { operate(true, value); }
   bool contains(const std::string &value) const;
 };
 
 // n must be greater than 0, p must be within range, 0 -> 1
-BloomFilter::BloomFilter(int n, double p)
-    : bit_array_size(
+CountingBloomFilter::CountingBloomFilter(int n, double p)
+    : array_size(
           static_cast<int>(std::ceil(-((n * std::log(p)) / LN_2_SQUARED)))),
-      num_hashes(
-          static_cast<int>(std::ceil(((double)bit_array_size / n) * LN_2))),
-      buckets((bit_array_size + 63) / 64), bits(buckets) {
+      num_hashes(static_cast<int>(std::ceil(((double)array_size / n) * LN_2))),
+      buckets((array_size + 7) / 8), bits(buckets) {
   if (n <= 0 || p <= 0.0 || p >= 1.0) {
     throw std::invalid_argument("invalid input: n must be > 0, 0 < p < 1");
   }
 }
 
 std::vector<uint64_t>
-BloomFilter::get_hash_indexes(const std::string &value) const {
+CountingBloomFilter::get_hash_indexes(const std::string &value) const {
   std::vector<uint64_t> indexes;
   indexes.reserve(num_hashes);
 
@@ -71,30 +73,41 @@ BloomFilter::get_hash_indexes(const std::string &value) const {
   uint64_t h2 = std::hash<std::string>{}("salt" + value);
 
   for (int i = 0; i < num_hashes; i++) {
-    indexes.emplace_back((h1 + i * h2) % bit_array_size);
+    indexes.emplace_back((h1 + i * h2) % array_size);
   }
 
   return indexes;
 }
 
-void BloomFilter::insert(const std::string &value) {
+void CountingBloomFilter::operate(bool remove, const std::string &value) {
   std::vector<uint64_t> indexes = get_hash_indexes(value);
   for (uint64_t index : indexes) {
-    int bucket = index / 64;
-    int index_in_bucket = index % 64;
-    bits[bucket] |= (1ULL << index_in_bucket);
+    int bucket = index / 8;
+    int offset = (index % 8) * 8;
+
+    uint8_t count = (bits[bucket] >> offset) & 0xFF;
+
+    if (remove && count > 0)
+      count--;
+    else if (!remove && count != 255)
+      count++;
+    else
+      continue;
+
+    bits[bucket] &= ~(0xFFULL << offset);
+    bits[bucket] |= (static_cast<uint64_t>(count) << offset);
   }
 }
 
-bool BloomFilter::contains(const std::string &value) const {
+bool CountingBloomFilter::contains(const std::string &value) const {
   std::vector<uint64_t> indexes = get_hash_indexes(value);
   for (uint64_t index : indexes) {
-    int bucket = index / 64;
-    int index_in_bucket = index % 64;
+    int bucket = index / 8;
+    int offset = (index % 8) * 8;
 
-    if ((bits[bucket] & (1ULL << index_in_bucket)) == 0) {
+    uint8_t count = (bits[bucket] >> offset) & 0xFF;
+    if (count == 0)
       return false;
-    }
   }
   return true;
 }
