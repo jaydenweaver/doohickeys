32f377075e084837ea5002a0255947172d64b6d8
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-02 02:12:10 +1000

    add insert()

--- data-structures/SkipList.hpp
+++ data-structures/SkipList.hpp
@@ -14,22 +14,27 @@
 #pragma once
 
 #include <random>
+#include <type_traits>
 #include <vector>
 
 template <typename T> struct Node {
-  int key;
-  std::vector<Node *> forward;
+  T key;
+  std::vector<Node<T> *> forward;
 
-  Node(int key, int level) : key(key), forward(level, nullptr) {}
+  Node(const T &key, int level) : key(key), forward(level, nullptr) {}
 };
 
 template <typename T> class SkipList {
+  static_assert(std::is_convertible_v<
+                    decltype(std::declval<T>() < std::declval<T>()), bool>,
+                "type T must support < operator");
+
 private:
   const double P = 0.5;
   const int MAX_LEVEL = 15;
 
   int level = 0;
-  Node<T> *head;
+  Node<T> *head = new Node<T>(T{}, MAX_LEVEL);
 
   int assign_level();
 
@@ -37,13 +42,13 @@ public:
   SkipList();
   ~SkipList();
 
-  void insert(int key);
-  void remove(int key);
-  Node<T> *search(int key);
+  void insert(const T &key);
+  void remove(const T &key);
+  Node<T> *search(const T &key);
 };
 
 template <typename T> int SkipList<T>::assign_level() {
-  int level = 0;
+  int level = 1;
 
   std::random_device rand;
   std::mt19937 gen(rand());
@@ -53,4 +58,33 @@ template <typename T> int SkipList<T>::assign_level() {
     level++;
 
   return level;
+}
+
+template <typename T> void SkipList<T>::insert(const T &key) {
+  int level = assign_level();
+
+  if (head == nullptr) {
+    head = new Node<T>(key, level);
+    return;
+  }
+
+  Node<T> *curr = head;
+  std::vector<Node<T> *> list(this->level, nullptr);
+
+  for (int i = level - 1; i >= 0; i--) {
+    while (curr->forward[i] && key > curr->forward[i]->key) {
+      curr = curr->forward[i];
+    }
+    list[i] = curr;
+  }
+
+  Node<T> *node = new Node<T>(key, level);
+
+  for (int i = 0; i < level; i++) {
+    node->forward[i] = list[i]->forward[i];
+    list[i]->forward[i] = node;
+  }
+
+  if (level > this->level)
+    this->level = level;
 }
